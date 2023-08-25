7d7eabc8956c5def44ab612fbafc7f162abe48c1
anastasovv <simon@digitalnode.com>
2023-08-25 18:50:32 +0200

    Add switch for global / segment by segment analysis

---
+++ frontend/src/components/results/SegmentBySegmentAnalysisResults.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SegmentBySegmentAnalysisResults = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default SegmentBySegmentAnalysisResults
