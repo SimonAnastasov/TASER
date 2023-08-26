bae061896668eb4e382f519fec5616efc13e5802
anastasovv <simon@digitalnode.com>
2023-08-26 15:39:13 +0200

    Add showing of segment by segment analysis

--- frontend/src/components/results/GlobalAnalysisResults.jsx
+++ frontend/src/components/results/GlobalAnalysisResults.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-import InfoButton from '../utils/InfoButton'
-import { INFO_BOXES_ARE_SCROLLABLE } from '../../utils/infoTexts'
 import SentimentIcon from '../utils/SentimentIcon'
+import InfoBoxes from './InfoBoxes'
 
 const GlobalAnalysisResults = () => {
     const analysisResult = useSelector(state => state?.analysisResult?.result)
@@ -18,77 +17,15 @@ const GlobalAnalysisResults = () => {
 
     return (
         <>
+            {/* Show global sentiment */}
             <div className="mt-8 flex gap-2 justify-center items-center">
                 <SentimentIcon sentiment={analysisResult.Global_sentiment}/>
                 <span className="heading--5">Sentiment: {analysisResult.Global_sentiment[0].toUpperCase() + analysisResult.Global_sentiment.slice(1)}</span>
                 <SentimentIcon sentiment={analysisResult.Global_sentiment}/>
             </div>
 
-            <div className="lg:px-6 mx-auto mt-8 grid grid-cols-1 lg:grid-cols-3 gap-y-8 lg:gap-8">
-
-                {/* Container on the left side of the global analysis dashboard */}
-                <div className="relative bg-black/80 text-white shadow-md max-h-[400px] lg:max-h-[832px] overflow-y-scroll">
-                    <div className="sticky top-0 bg-black p-4 shadow-md">
-                        <div className="w-fit">
-                            <p className="heading--5">Entity Appearances</p>
-                        </div>
-                    </div>
-                    
-                    <div className="py-2 px-4">
-                        {/* A single entity_type holds many entities  */}
-                        {/* Global_entities_list is 'key (entity_type) - value (list of entity objects)' object */}
-                        {/* A single entity is 'key - value' object */}
-                        {Object.entries(analysisResult.Global_entities_list).map(([entity_type, entities]) => (
-                            <div key={entity_type} className="mb-10">
-                                <div className="w-fit">
-                                    <span className="heading--6">{entity_type}</span>
-                                    <hr/>
-                                </div>
-                                <div className="mt-4 pl-4">
-                                    {/* Sort entities so that the one with most appearances is on top */}
-                                    {[...entities].sort((a, b) => Object.values(b)[0] - Object.values(a)[0]).map((entity, index) => (
-                                        <div key={`entity_${index}`}>
-                                            <span className="heading--6 flex gap-2 justify-between items-end mb-3 border-y border-white/20">
-                                                <span className="block leading-7">{Object.keys(entity)[0]}:</span>
-                                                <span className="block leading-7">{Object.values(entity)[0]}</span>
-                                            </span>
-                                        </div>
-                                    ))}
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-
-                {/* Container on the right side of the global analysis dashboard */}
-                <div className="col-span-2 grid grid-cols-1 lg:grid-rows-2 gap-8">
-                    {global_texts.map((text, index) => (
-                        <div key={`container_texts_${index}`}>
-                            {/* Content container */}
-                            <div key={`global_text_${index}`} className="relative bg-black/80 text-white shadow-md max-h-[400px] overflow-y-scroll">
-                                {/* Sticky overlay on top holding the title */}
-                                <div className="sticky top-0 bg-black p-4 shadow-md">
-                                    <div className="w-fit">
-                                        <p className="heading--5">{text.title}</p>
-                                    </div>
-                                </div>
-                                {/* The content */}
-                                <div className="heading--6 flex gap-2">
-                                    {/* Add numbers to indicate which line of text is being read */}
-                                    <div className="flex-shrink-0 bg-black py-2 px-2 shadow-md flex flex-col">
-                                        {Array.from({ length: Math.floor(text.content.length / 20) }, (_, i) => i + 1).map((num) => (
-                                            <span key={num} className="text-center">{num}</span>
-                                        ))}
-                                    </div>
-                                    {/* Add the content */}
-                                    <div className="py-2 pl-2 pr-8 text-justify">{text.content}</div>
-                                </div>
-                            </div>
-                            {/* End content container */}
-                        </div>
-                    ))}
-                </div>
-            </div>
+            {/* Show info boxes */}
+            <InfoBoxes entities_list={analysisResult.Global_entities_list} global_texts={global_texts}/>
         </>
     )
 }
--- frontend/src/components/results/SegmentBySegmentAnalysisResults.jsx
+++ frontend/src/components/results/SegmentBySegmentAnalysisResults.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 
+import SentimentIcon from '../utils/SentimentIcon';
+import InfoButton from '../utils/InfoButton';
+import InfoBoxes from './InfoBoxes';
+
 const SegmentBySegmentAnalysisResults = () => {
     const analysisResult = useSelector(state => state?.analysisResult?.result)
+    const totalSegments = analysisResult.segments.length;
 
     const [shownSegmentNumber, setShownSegmentNumber] = useState(0)
     
@@ -13,9 +18,65 @@ const SegmentBySegmentAnalysisResults = () => {
         <>
             {analysisResult.segments.map((segment, index) => (
                 <div key={`segment${index}`} className={`${index === shownSegmentNumber ? '' : 'hidden'}`}>
-                    <div className="lg:px-6 mx-auto mt-8 grid grid-cols-1 lg:grid-cols-3 gap-y-8 lg:gap-8">
-                        Test
+                    {/* Switch between segments with back and forward arrows */}
+                    <div className="lg:px-6 w-fit mx-auto mt-16 flex justify-center items-center gap-4 lg:gap-12">
+                        <div className="text-2xl w-fit py-2 px-4 rounded shadow-md bg-white text-black cursor-pointer hover:bg-black hover:text-white transition-all duration-300"
+                             onClick={() => setShownSegmentNumber( (totalSegments + shownSegmentNumber - 1) % totalSegments )}
+                        >
+                            ←
+                        </div>
+                        <div className="">
+                            Segment {index + 1}/{totalSegments}
+                        </div>
+                        <div className="text-2xl w-fit py-2 px-4 rounded shadow-md bg-white text-black cursor-pointer hover:bg-black hover:text-white transition-all duration-300"
+                             onClick={() => setShownSegmentNumber( (totalSegments + shownSegmentNumber + 1) % totalSegments )}
+                        >
+                            →
+                        </div>
                     </div>
+
+                    {/* Show speaker */}
+                    <div className="mt-8 flex gap-2 justify-center items-center">
+                        <span className="heading--6">Speaker: <span className="font-bold">{segment.speaker}</span></span>
+                    </div>
+
+                    {/* Show time frame of speaking */}
+                    <div className="mt-2 w-fit mx-auto">
+                        <div className="flex justify-center items-center gap-4">
+                            <div className="flex flex-col items-center gap-1">
+                                <InfoButton infoText={"The timestamp in the audio when the speaker in this segment started speaking."} small/>
+                                <div className="heading--3">🎬</div>
+                                <div className="heading--6">{segment.start.toFixed(2)}s</div>
+                            </div>
+                            <div className="h-0.5 w-32 bg-black -translate-y-3"></div>
+                            <div className="flex flex-col items-center gap-1">
+                                <InfoButton infoText={"The total duration for which the speaker in this segment was speaking."} small/>
+                                <div className="heading--3">⏳</div>
+                                <div className="heading--6">{(segment.end - segment.start).toFixed(2)}s</div>
+                            </div>
+                            <div className="h-0.5 w-32 bg-black -translate-y-3"></div>
+                            <div className="flex flex-col items-center gap-1">
+                                <InfoButton infoText={"The timestamp in the audio when the speaker in this segment finished speaking."} small/>
+                                <div className="heading--3">🏁</div>
+                                <div className="heading--6">{segment.end.toFixed(2)}s</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Show sentiment */}
+                    <div className="mt-2 flex gap-2 justify-center items-center">
+                        <SentimentIcon sentiment={segment.sentiment}/>
+                        <span className="heading--6">Sentiment: {segment.sentiment[0].toUpperCase() + segment.sentiment.slice(1)}</span>
+                        <SentimentIcon sentiment={segment.sentiment}/>
+                    </div>
+
+                    {/* Show info boxes */}
+                    <InfoBoxes  entities_list={segment.entities} 
+                                global_texts={[
+                                    {title: "Transcription Text", content: segment.text},
+                                    {title: "Abstractive Summarization", content: segment.abstractive_summarization_text}
+                                ]}
+                    />
                 </div>
             ))}
         </>
