741be60c5996f3e4c74da199b79b0e981a01c71c
anastasovv <simon@digitalnode.com>
2023-08-30 18:33:34 +0200

    Configure CORS to allow all origins on all endpoints

--- backend/src/main/kotlin/amon/taser/TaserApplication.kt
+++ backend/src/main/kotlin/amon/taser/TaserApplication.kt
@@ -5,12 +5,27 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class TaserApplication {
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder(10)
+    }
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+            }
+        }
     }
 }
 
