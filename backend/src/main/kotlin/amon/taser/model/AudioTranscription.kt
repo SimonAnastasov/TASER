5d55e4729d613e4b6246fd8b72a8aba6f67a047d
anastasovv <simon@digitalnode.com>
2023-09-02 12:03:16 +0200

    Add timestamp_created and timestamp_updated to tables

--- backend/src/main/kotlin/amon/taser/model/AudioTranscription.kt
+++ backend/src/main/kotlin/amon/taser/model/AudioTranscription.kt
@@ -1,6 +1,7 @@
 package amon.taser.model
 
 import jakarta.persistence.*
+import java.time.Instant
 import java.util.UUID
 
 @Entity
@@ -9,7 +10,13 @@ class AudioTranscription(
     val filename: String,
     val isCompleted: Boolean = false,
     @ManyToOne val user: User?,
-    @Id @GeneratedValue(strategy = GenerationType.UUID) val id: UUID?
+    @Id @GeneratedValue(strategy = GenerationType.UUID) val id: UUID?,
+
+    @Column(name = "timestamp_created", columnDefinition = "TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP")
+    val timestampCreated: Instant = Instant.now(),
+
+    @Column(name = "timestamp_updated", columnDefinition = "TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP")
+    val timestampUpdated: Instant = Instant.now()
 ) {
     fun copy(
             text: String = this.text,
--- backend/src/main/kotlin/amon/taser/model/User.kt
+++ backend/src/main/kotlin/amon/taser/model/User.kt
@@ -1,6 +1,7 @@
 package amon.taser.model
 
 import jakarta.persistence.*
+import java.time.Instant
 import java.util.UUID
 
 @Entity
@@ -9,5 +10,11 @@ data class User(
         val username: String,
         val password: String,
         val email: String,
-        @Id @GeneratedValue(strategy = GenerationType.UUID) val id: UUID? = null
+        @Id @GeneratedValue(strategy = GenerationType.UUID) val id: UUID? = null,
+        
+        @Column(name = "timestamp_created", columnDefinition = "TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP")
+        val timestampCreated: Instant = Instant.now(),
+
+        @Column(name = "timestamp_updated", columnDefinition = "TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP")
+        val timestampUpdated: Instant = Instant.now()
 )
