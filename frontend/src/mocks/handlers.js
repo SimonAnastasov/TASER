b00e6b8ae763e0d37129f4bff4e7477a4ccb5d66
andrejdaskalov <41647331+andrejdaskalov@users.noreply.github.com>
2023-09-24 21:35:19 +0200

    working api mock service worker

--- frontend/src/mocks/handlers.js
+++ frontend/src/mocks/handlers.js
@@ -1,2 +1,28 @@
 import { rest } from 'msw'
 
+const baseUrl = 'http://localhost:8080/api'
+
+export const handlers = [
+    rest.post('baseUrl+/login', (req, res, ctx) => {
+        const { username, password } = req.body
+        if (username === 'testuser' && password === 'testuser') {
+            return res(
+                ctx.status(200),
+                ctx.json({
+                    bearerToken: 'mocked_user_token',
+                }),
+            )
+        } else {
+            return res(
+                ctx.status(400),
+                ctx.json({
+                    "error": true,
+                    "message": "You entered invalid credentials."
+                }),
+            )
+        }
+    }),
+
+    
+
+]
