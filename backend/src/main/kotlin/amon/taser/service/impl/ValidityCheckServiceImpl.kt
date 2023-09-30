5a76f0c34268de977eda1008ce3703e03664c3e5
andrejdaskalov <41647331+andrejdaskalov@users.noreply.github.com>
2023-09-30 20:42:03 +0200

    added check for review length discrepancies;

--- backend/src/main/kotlin/amon/taser/service/impl/ValidityCheckServiceImpl.kt
+++ backend/src/main/kotlin/amon/taser/service/impl/ValidityCheckServiceImpl.kt
@@ -2,14 +2,14 @@ package amon.taser.service.impl
 
 import amon.taser.service.ValidityCheckService
 import org.springframework.stereotype.Service
-import java.util.*
+import kotlin.math.abs
 
 
 @Service
 class ValidityCheckServiceImpl: ValidityCheckService {
     override fun checkReviewDifference(review: String, original: String): Boolean {
-        val difference = jaccardIndex(review, original)
-        return difference <= original.length * 0.25
+        val similarity = jaccardIndex(review, original)
+        return similarity >= 0.85 && abs(review.length - original.length) <= 500
     }
     fun jaccardIndex(a: String, b: String): Double {
         val aSet = a.split(" ").toSet()
